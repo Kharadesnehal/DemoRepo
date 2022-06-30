0d11c7dc070dfd53c824b8d543aff1d7b7e10720
Snehal Kharade <snehal.kharade91@gmail.com>
2022-06-30 16:59:28 +0530

    first commit

---
+++ DemoJava/src/Test/arithmatic.java
@@ -0,0 +1,17 @@
+package Test;
+public class arithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a ,b;
+		a=100;
+		b=20;
+		int sum=a+b;
+		System.out.println("Addition is\t"+sum);
+		System.out.println("subtraction is\t"+(a-b));
+		System.out.println("multiplication is\t"+(a*b));
+		System.out.println("division is\t"+(a/b));
+		System.out.println("modulus is\t"+(a%b));
+	}
+
+}
